caa8625f1d43497e7ff5dc05d44af42da24a8743
IgnacioCava <cavaignacio11@gmail.com>
2022-12-12 22:25:18 -0300

    build: first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	<DropDown v-bind:options="['a', 'b']" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import DropDown from './components/DropDown.vue'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+	name: 'App',
+	components: {
+		DropDown
+	}
+})
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/DropDown.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		Department
+		<ul>
+			<li v-for="opt in options" :key="opt" class="text-3xl">
+				{{ opt }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+	name: 'DropDown',
+	props: {
+		options: Array as PropType<string[]>
+	}
+})
+</script>
